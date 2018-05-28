a755b9d21cbff587c7390b5bf17e4582ce0bb78f
Andrey Hernandez <hec_boy@hotmail.com>
2018-05-28 01:13:28 -0500

    Isercion de la primera hoja

---
+++ Diccionario de Datos/Arbol.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diccionario_de_Datos
+{
+    class Arbol
+    {
+        public long direccion { get; set; }
+        public char tipo { get; set; }
+        public long[] apuntador { get; set; }
+        
+        public int[]  clave { get; set; }
+        public int tam { get; set; }
+
+        public Arbol()
+        {
+            direccion = -1;
+            tipo = 'H';
+            apuntador = new long[4];
+            clave = new int[4];
+            tam = 0;
+
+        }
+        
+        
+    }
+}
